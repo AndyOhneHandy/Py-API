7069e10515245e263e5cbdafed622e5cec955392
Andreas Lercher <andreas.lercher.01@gmail.com>
2021-08-24 16:39:20 +0200

    Added variables

--- messagecheck.py
+++ messagecheck.py
@@ -9,14 +9,15 @@ B90Kw = ["die grünen", "bündnis 90", "baerbock", "bärbock", "habeck", "kretsc
 LinKw = ["die linke", "linkspartei", "wissler", "wellsow"]
 
 triggerwords = AfDKw + FDPKw + CDUKw + SPDKw + B90Kw + LinKw
-print(triggerwords)
+# print(triggerwords)
 posKw = ["gut", "toll", "wunderbar", ]
 negKw = ["schlecht", "blöd", "nervig", ]
-unklarKw = ["nicht", "keinesfalls"]
+unklarKw = ["nicht ", "keinesfalls"]
 
-for trigger in triggerwords:
-    if trigger in testinput:
-        print("Es klappt!")
+commentcount = 0
+polcommentcount = 0
+
+
 
 def konnotationcheck(testinput):
     poscount = 0
@@ -39,7 +40,10 @@ def konnotationcheck(testinput):
             pncount  = pncount*(-1)
             unklar = 1
     
-    konnotationvalue = round(pncount/wordcount, 2)
+    if wordcount == 0:
+        konnotationvalue = 0
+    else:
+        konnotationvalue = round(pncount/wordcount, 2)
 
     return konnotationvalue, unklar
 
@@ -55,40 +59,75 @@ def konnotationcheck(testinput):
 
 def partycheck(testinput):
     afdDummy = 0
-    fdpDummy = 0 
+    aAbs = 0
+    fdpDummy = 0
+    fAbs = 0
     cduDummy = 0
+    cAbs = 0
     spdDummy = 0
+    sAbs = 0
     b90Dummy = 0
+    gAbs = 0
     linDummy = 0
+    lAbs = 0
     multipdummy = 0
+    triggerwordlist = ""
+    triggerwordcount = 0
     
     for afdk in AfDKw:
         if afdk in testinput:    
             afdDummy = 1
+            aAbs += 1
+            triggerwordlist += afdk + ";"
+
 
     for fdpk in FDPKw:
         if fdpk in testinput:    
             fdpDummy = 1
+            fAbs += 1
+            triggerwordlist += fdpk + ";"
+
 
     for cduk in CDUKw:
         if cduk in testinput:    
             cduDummy = 1
+            cAbs += 1
+            triggerwordlist += cduk + ";"
+
 
     for spdk in SPDKw:
         if spdk in testinput:    
             spdDummy = 1
+            sAbs += 1
+            triggerwordlist += spdk + ";"
+
 
     for b90k in B90Kw:
         if b90k in testinput:    
             b90Dummy = 1
+            gAbs += 1
+            triggerwordlist += b90k + ";"
+
 
     for link in LinKw:
         if link in testinput:    
             linDummy = 1
+            lAbs += 1
+            triggerwordlist += link + ";"
+
+
+    triggerwordcount = aAbs + fAbs + cAbs + sAbs + gAbs + lAbs
 
     if afdDummy + fdpDummy + cduDummy + spdDummy + b90Dummy + linDummy > 1:
         multipdummy = 1
-    return afdDummy, fdpDummy, cduDummy, spdDummy, b90Dummy, linDummy, multipdummy
+    return afdDummy, fdpDummy, cduDummy, spdDummy, b90Dummy, linDummy, multipdummy, triggerwordlist, aAbs, fAbs, cAbs, sAbs, gAbs, lAbs, triggerwordcount
     
-print(partycheck(testinput))
-print(konnotationcheck(testinput))
+
+for trigger in triggerwords:
+    if trigger in testinput:
+        commentcount += 1
+        polcommentcount += 1
+        print(partycheck(testinput)+konnotationcheck(testinput))
+    else:
+        commentcount += 1
+
